451baeaa543a0ce58b04a6c338b48dac5e99b1fc
shivamp01 <shivampadmawar001@gmail.com>
2021-04-05 15:08:07 +0530

    Self auto commit

---
+++ mvn-demo/src/test/java/com/ibm/demo/TestMaths.java
@@ -0,0 +1,40 @@
+package com.ibm.demo;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestMaths {
+	
+	private static Maths m;
+	
+	@BeforeAll
+	public static void setup() {
+		m=new Maths();
+	}
+	
+	@Test
+	public void testAdd() {
+		assertEquals(10, m.add(5, 5));
+	}
+	
+	@Test
+	public void testSubs() {
+		assertEquals(8, m.subs(10, 2));
+	}
+	
+	@Test
+	public void testDiv() {
+		assertThrows(ArithmeticException.class, ()->m.div(2, 0));
+	}
+	
+	@Test
+	public void testSquare() {
+		assertEquals(25, m.square(5));
+	}
+	
+}
